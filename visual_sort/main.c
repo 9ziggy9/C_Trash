2d4fbb9551f14991d78d56564e0dd4ada452caf4
PapaZigs <davidarogers@protonmail.com>
2021-02-16 19:43:42 -0800

    Insertion algorithm added for testing; TTF pre-loaded. Color and sound to do. Werks.

--- visual_sort/main.c
+++ visual_sort/main.c
@@ -1,17 +1,20 @@
+// SDL2 libraries
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2_gfxPrimitives.h>
+#include <SDL_ttf.h>
+// Standard Libraries
 #include <unistd.h>
 #include <time.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+// MACROS: remember to replace with getopts
 #define WINDOW_WIDTH 960
 #define WINDOW_HEIGHT 640
-#define NUM 32
+#define NUM 96
 
 bool running;
 
@@ -29,6 +32,17 @@ void input()
 	const Uint8 *keystates = SDL_GetKeyboardState(NULL);
 	if(keystates[SDL_SCANCODE_ESCAPE])
 		running = false;
+}
+
+void render_backsplash()
+{
+	// backsplash of window, gruxbox gray
+	SDL_SetRenderDrawColor(rend, 40, 40, 40, 255);
+	SDL_Rect rect;
+	rect.x = rect.y = 0;
+	rect.w = WINDOW_WIDTH;
+	rect.h = WINDOW_HEIGHT;
+	SDL_RenderFillRect(rend, &rect);
 }
 
 void populate_segment_array(int *array)
@@ -74,15 +88,33 @@ void draw_segments(int *height)
 	}
 }
 
+void draw_initially(int *height)
+{
+	int SEGMENT_WIDTH = WINDOW_WIDTH / NUM;
+	
+	render_backsplash();
+	
+	// foreground, segment struct, gruvbox
+	SDL_Rect segment;
+	SDL_SetRenderDrawColor(rend, 251, 241, 199, 255);
+	segment.y = WINDOW_HEIGHT;
+	segment.w = SEGMENT_WIDTH;
+
+	for (int n=0; n<NUM; n++) 
+	{
+		segment.x = n * SEGMENT_WIDTH;
+		segment.h = - height[n]; // NOTE: direction of increasing y reversed in SDL
+		SDL_RenderFillRect(rend, &segment); // draw
+		SDL_RenderPresent(rend);
+		// added delay for step effect, usleep is actually DEPRECATED now,
+		// please return and replace with nanosleep()!
+		usleep(15000);
+	}
+}
+
 void draw_state(int *height) 
 {
-	// backsplash of window, gruxbox gray
-	SDL_SetRenderDrawColor(rend, 40, 40, 40, 255);
-	SDL_Rect rect;
-	rect.x = rect.y = 0;
-	rect.w = WINDOW_WIDTH;
-	rect.h = WINDOW_HEIGHT;
-	SDL_RenderFillRect(rend, &rect);
+	render_backsplash();
 
 	// draw current array sequence
 	draw_segments(height);
@@ -97,9 +129,33 @@ void draw_state(int *height)
 	SDL_RenderPresent(rend);
 }
 
+/////////////////////////
+// SORTING ALGORITHMS //
+////////////////////////
+
+// Insertion Sort
+void insertionSort(int array[], int n) 
+{ 
+	int i, element, j; 
+	
+	for (i = 1; i < n; i++)
+	{ 
+		element = array[i]; j = i - 1; 
+		while (j >= 0 && array[j] > element) 
+		{ 
+			array[j + 1] = array[j]; 
+			j = j - 1; 
+		} 
+		array[j + 1] = element; 
+		draw_state(array);
+		usleep(50000);
+	} 
+} 
+
 int main()
 {
 	running = 1; // running state is acted upon by input()
+	bool SORT_COMPLETE = false;
 	int segment_height[NUM]; // array of segment heights, primary argument
 	
 	// SDL2 initialization
@@ -111,10 +167,20 @@ int main()
 	SDL_ShowCursor(0);
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "2");
 
-	// INITIAL CONDITIONS: Seed RNG, populate array and apply Fisher-Yates shuffle
+	// message box surface/texture initialization
+	if(TTF_Init()==-1) 
+	{
+    printf("TTF_Init: %s\n", TTF_GetError());
+    exit(2);
+  }
+
+	// INITIAL CONDITIONS: Seed RNG, populate array and apply Fisher-Yates shuffle.
+	// Please note that draw_initially() contains a usleep() call. usleep() is
+	// now deprecated and should be replaced ny nanosleep() in the future.
 	srand(time(NULL));
 	populate_segment_array(segment_height);
 	shuffle_segments(segment_height, NUM);
+	draw_initially(segment_height);
 
 	// RUNNING STATE //
 	while (running) 
@@ -126,15 +192,15 @@ int main()
 			fps = frameCount;
 			frameCount = 0;
 		}
-		
-		// print statement for fps debugging.
-		printf("%d\n", fps);
 
 		// look for input.
 		input();
+		
+		if(!SORT_COMPLETE) {
+			insertionSort(segment_height, NUM);
+			SORT_COMPLETE = true;
+		}
 
-		// draw state to frame
-		draw_state(segment_height);
 	}
 
 	SDL_DestroyRenderer(rend);
